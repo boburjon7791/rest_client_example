390322bb048c2e59415efe2ab5a475f88c3c2f95
Boburjon <soliyevboburjon95@gmail.com>
2024-08-20 16:03:55 +0500

    Removed model, controller and service classes

--- src/main/java/com/rest_client/rest_client_example/RestClientExampleApplication.java
+++ src/main/java/com/rest_client/rest_client_example/RestClientExampleApplication.java
@@ -1,14 +1,7 @@
 package com.rest_client.rest_client_example;
 
-import com.rest_client.rest_client_example.client.RestNetwork;
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpHeaders;
-import org.springframework.util.LinkedMultiValueMap;
-
-import java.util.List;
-import java.util.Map;
 
 @SpringBootApplication
 public class RestClientExampleApplication {
