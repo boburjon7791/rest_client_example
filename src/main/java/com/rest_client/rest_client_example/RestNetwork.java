a006b21759f70a6d773b61d125c241482b170416
Soliyev Boburjon <122117930+boburjon7791@users.noreply.github.com>
2024-08-20 16:41:51 +0500

    Create RestNetwork.java

---
+++ src/main/java/com/rest_client/rest_client_example/RestNetwork.java
@@ -0,0 +1,78 @@
+package com.rest_client.rest_client_example.client;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rest_client.rest_client_example.utils.Utils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClient;
+
+import java.util.Map;
+
+public class RestNetwork {
+    private static final RestClient restClient=RestClient.builder().build();
+    private static final ObjectMapper objectMapper=new ObjectMapper();
+
+    public static <T> String get(String apiUrl, Map<String, ?> params){
+        return restClient.get()
+                .uri(apiUrl, params)
+                .retrieve()
+                .onStatus(Utils.okErrorChecker, Utils.globalErrorHandler)
+                .onStatus(Utils.okSuccessChecker, Utils.globalSuccessHandler)
+                .body(String.class);
+    }
+
+    public static <T> String post(String apiUrl, T body){
+        return restClient.post()
+                .uri(apiUrl)
+                .body(body)
+                .contentType(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .onStatus(Utils.okErrorChecker, Utils.globalErrorHandler)
+                .onStatus(Utils.okSuccessChecker, Utils.globalSuccessHandler)
+                .body(String.class);
+    }
+
+    public static <T> String post(String apiUrl, T body, HttpHeaders inputHeaders){
+        return restClient.post()
+                .uri(apiUrl)
+                .body(body)
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(httpHeaders -> httpHeaders.addAll(inputHeaders))
+                .retrieve()
+                .onStatus(Utils.okErrorChecker, Utils.globalErrorHandler)
+                .onStatus(Utils.okSuccessChecker, Utils.globalSuccessHandler)
+                .body(String.class);
+    }
+
+    public static <T> String post(String apiUrl, T body, Map<String, ?> params){
+        return restClient.post()
+                .uri(apiUrl, params)
+                .body(body)
+                .contentType(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .onStatus(Utils.okErrorChecker, Utils.globalErrorHandler)
+                .onStatus(Utils.okSuccessChecker, Utils.globalSuccessHandler)
+                .body(String.class);
+    }
+
+    public static String post(String apiUrl, MultiValueMap<String, String> formData){
+        return restClient.post()
+                .uri(apiUrl)
+                .body(formData)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .retrieve()
+                .onStatus(Utils.okErrorChecker, Utils.globalErrorHandler)
+                .onStatus(Utils.okSuccessChecker, Utils.globalSuccessHandler)
+                .body(String.class);
+    }
+
+    public static <T> T parseFromJson(String jsonBody, Class<T> type){
+        try {
+            return objectMapper.readValue(jsonBody, type);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
