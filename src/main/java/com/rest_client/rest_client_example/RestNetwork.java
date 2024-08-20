be850d4856555a435736446463f1da9538d2a2a8
Soliyev Boburjon <122117930+boburjon7791@users.noreply.github.com>
2024-08-20 16:43:32 +0500

    Update RestNetwork.java

--- src/main/java/com/rest_client/rest_client_example/RestNetwork.java
+++ src/main/java/com/rest_client/rest_client_example/RestNetwork.java
@@ -1,4 +1,4 @@
-package com.rest_client.rest_client_example.client;
+package com.rest_client.rest_client_example;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
